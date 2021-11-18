ac669b58eb548d4ce099736ff02db6dcd379694a
Michael Kriese <michael.kriese@visualon.de>
2021-11-18 16:35:17 +0100

    feat: add test

--- test/Tests.cs
+++ test/Tests.cs
@@ -1,3 +1,4 @@
+using NiL.JS.BaseLibrary;
 using NiL.JS.Core;
 using NiL.JS.Core.Interop;
 using NiL.JS.Extensions;
@@ -5,6 +6,7 @@ using NUnit.Framework;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 
 namespace lib.test
 {
@@ -50,6 +52,11 @@ namespace lib.test
             public string Title => "title";
 
             public bool Update() => true;
+        }
+
+        class ByteTest
+        {
+            public byte[] Data { get; set; }
         }
 
         [Test]
@@ -102,6 +109,26 @@ namespace lib.test
             Assert.AreEqual("title", se.Eval("it.Title").ToString());
             //Assert.IsNotNull(se.Eval("JSON.stringify(it)").Value);
             Assert.Throws<JSException>(() => se.Eval("JSON.stringify(it)"));
+            se.Eval("console.log('end')");
+        }
+
+        [Test]
+        public void Test_Issue_263_bytes()
+        {
+            // https://github.com/nilproject/NiL.JS/issues/263
+            var se = new Context();
+            ByteTest it = new ByteTest();
+            se.DefineVariable("it").Assign(JSValue.Marshal(it));
+            se.DefineVariable("buff").Assign(new ArrayBuffer(Encoding.UTF8.GetBytes("Test")));
+            se.Eval("console.log('start')");
+            se.Eval("console.log('buff', buff)");
+            se.Eval("console.log('it', it)");
+
+            se.Eval("it.Data = buff;");
+            se.Eval("console.log('it.Data', it.Data)");
+
+            Assert.That(it.Data, !Is.Null);
+
             se.Eval("console.log('end')");
         }
     }
